97112e8ace69a8ffb5b5202988963f14f38b4564
ajimura <ajimuralk@gmail.com>
2019-03-14 19:40:59 -0700

    Folder cleanup

--- backend/bookSearch.js
+++
@@ -1,4 +0,0 @@
-module.exports = bookSearch = input => {
-  const searchInput = input.split(' ').join('+');
-  return `https://www.googleapis.com/books/v1/volumes?q=${searchInput}&maxResults=40&orderBy=relevance`;
-};
